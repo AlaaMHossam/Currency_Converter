ba78768d9311cf60be5aca44186bcdd4c7355a62
Alaa Hossam <amr.hossam@gmail.com>
2022-03-15 14:40:37 +0200

    - Work on main

--- app/src/main/java/com/alaa/currencyconverter/ui/MainActivity.kt
+++ app/src/main/java/com/alaa/currencyconverter/ui/MainActivity.kt
@@ -1,15 +1,16 @@
-package com.alaa.currencyconverter
+package com.alaa.currencyconverter.ui
 
 import android.os.Bundle
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.alaa.currencyconverter.R
 import com.alaa.currencyconverter.core.base.BaseActivity
 import com.alaa.currencyconverter.databinding.ActivityMainBinding
 
 class MainActivity : BaseActivity<ActivityMainBinding>() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
         installSplashScreen()
+        super.onCreate(savedInstanceState)
     }
 
     override fun getLayoutResourceId(): Int = R.layout.activity_main
